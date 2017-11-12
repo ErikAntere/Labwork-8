3713e8a53dbeae7a0b6cdde37f997059fc9e5fa0
ErikAntere <31687066+ErikAntere@users.noreply.github.com>
2017-11-12 21:23:29 +0200

    Update welcome.ts

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Login } from '../login/login';
 
 /**
  * Generated class for the WelcomePage page.
@@ -22,4 +23,7 @@ export class WelcomePage {
     console.log('ionViewDidLoad WelcomePage');
   }
 
+}
+export class nextPage {
+  nextPage = Login;
 }
